33554ecd9a41376a7b6ef475b1c664060b881daf
John Alexander Barreto <john.barreto22@hotmail.com>
2020-04-07 17:27:38 -0500

    Create install-powershell.sh

---
+++ install-powershell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Download ans install PowerShell
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+sudo apt-get update
